bd11f4cbf2c30d7808bd0f3b7bc3bd9ca24f63a9
louis sabri <louis.sabri@gmail.com>
2022-07-23 14:25:30 +1000

    set 5 refactoring

--- set5/exercise1.py
+++ set5/exercise1.py
@@ -25,37 +25,36 @@ Modify this function, don't write a whole new one.
 
 
 def wordy_pyramid():
-    baseURL = (
-        "https://us-central1-waldenpondpress.cloudfunctions.net/"
-        "give_me_a_word?wordlength={length}"
-    )
+
     pyramid_list = []
-    for i in range(3, 21, 2):
-        url = baseURL.format(length=i)
-        r = requests.get(url)
-        if r.status_code is 200:
-            message = r.text
-            pyramid_list.append(message)
-        else:
-            print("failed a request", r.status_code, i)
-    for i in range(20, 3, -2):
-        url = baseURL.format(length=i)
-        r = requests.get(url)
-        if r.status_code is 200:
-            message = r.text
-            pyramid_list.append(message)
-        else:
-            print("failed a request", r.status_code, i)
+    
+    list_1 = list_of_words_with_lengths(listoflengths = [3, 21, 2])
+    list_2 = list_of_words_with_lengths(listoflengths = [20, 3, -2])
+
+    pyramid_list = list_1 + list_2
 
     return pyramid_list
 
 
 def get_a_word_of_length_n(length):
-    pass
+    baseURL = (
+        "https://us-central1-waldenpondpress.cloudfunctions.net/"
+        "give_me_a_word?wordlength={length}"
+    )
 
+    url = baseURL.format(length = length)
+    r = requests.get(url)
+    if r.status_code is 200:
+            message = r.text
+            return message
 
 def list_of_words_with_lengths(list_of_lengths):
-    pass
+    pyramid_list = []
+
+    for length in range(list_of_lengths[0], list_of_lengths[1], list_of_lengths[2]):
+        pyramid_list.append(get_a_word_of_length_n(length))
+    
+    return pyramid_list
 
 
 if __name__ == "__main__":
